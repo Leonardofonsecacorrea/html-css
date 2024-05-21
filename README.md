ed3895202df245d30658cd47f391264c0e9884e5
Leonardofonsecacorrea <guns-leo@live.com>
2024-05-21 20:01:07 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# html-css
+ Estudos HTML curso gustavo guanabara
---
+++ Testes com ia/Pagina simples bttn whats/script.js
@@ -0,0 +1,3 @@
+document.querySelector('#cta').addEventListener('click', () => {
+    alert('Thank you for your interest! Our team will contact you soon.');
+});
